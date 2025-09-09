834b90726422ccb99c2c51b9268c25c11bd7a66c
BWIZA Annie <a.bwiza1@alustudent.com>
2025-09-09 08:18:03 -0700

    d

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-# This regex captures:
-# 1. sender: [from:...] 
-# 2. receiver: [to:...]
-# 3. flags: [flags:...]
-pattern = /\[from:([+\d]+)\].*?\[to:([+\d]+)\].*?\[flags:([-\d:]+)\]/ 
-
-ARGF.each_line do |line|
-  # Remove HTML entities like &nbsp; that may appear in logs
-  clean_line = line.gsub('&nbsp;', ' ')
-
-  if match = clean_line.match(pattern)
-    sender, receiver, flags = match.captures
-    # Print exactly in requested format: [SENDER],[RECEIVER],[FLAGS]
-    puts "#{sender},#{receiver},#{flags}"
-  end
-end
+puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
