485b489fd7423cd5a827c40ea8c15c49ff60e4a8
BWIZA Annie <a.bwiza1@alustudent.com>
2025-09-09 07:50:33 -0700

    add

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 
-pattern = /From:\s*(\+?\d+)\s*To:\s*(\+?\d+)\s*FLAGS:\s*([\w,]+)/
+# Regex pattern to match the "from", "to", and "flags" fields in the log
+pattern = /\[from:(\+?\d+)\]\s*\[to:(\+?\d+)\]\s*\[flags:([-\d:]+)\]/
 
 ARGF.each_line do |line|
+  # Try to match the pattern
   if match = line.match(pattern)
     sender, receiver, flags = match.captures
+    # Print in the requested format
     puts "#{sender},#{receiver},#{flags}"
   end
 end
